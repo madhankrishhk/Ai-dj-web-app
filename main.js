7ef06ad43db51ab303b5acdca8ef051de7c6bc34
madhankrishhk <84235355+madhankrishhk@users.noreply.github.com>
2022-05-11 16:23:54 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -19,12 +19,24 @@ function setup() {
 
 function draw() {
     image(video, 0, 0, 600, 500);
+    Fill("#FF0000");
+    stroke("FF0000");
+    if(scoreLeftWrist > 0.2)
+    {
+        circle(leftWristX,leftWristY,20);
+        InNumberleftWristY = Number(leftWristY);
+        remove_decimals = floor(InNumberleftWristY);
+        volume = remove_decimals/500;
+        document.getElementById("volume").innerHtml = "Volume = "+ volume;
+        song.setVolume(volume);
+    }
+   
 }
 
 function play() {
     song.play();
     song.setVolume(1);
-    son.rate(1);
+    song.rate(1);
 }
 
 function modelLoaded() {
@@ -33,8 +45,10 @@ function modelLoaded() {
 
 function gotPoses() {
     if (results.length > 0)
-        console.log(results)
-    eftWristX = results[0].pose.leftWrist.x;
+        console.log(results);
+        scoreLeftWrist = results(0).pose.keypoints[9].score;
+        console.log("scoreLeftWrist = " + scoreLetWrist);
+    LeftWristX = results[0].pose.leftWrist.x;
     leftWristY = results[0].pose.leftWrist.y;
     console.log("leftWristX = " + leftWristX + "leftWristY = " + leftWristY);
 
